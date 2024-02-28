603ef7a3665ca0e22f940ea55bf1e28958784abf
Alcarmen <alcarmenbrandon@gmail.com>
2024-02-29 00:29:21 +0800

    Innit_Progress
    Background images

--- PhotoAlbum_Alcarmen/obj/Debug/net8.0/PhotoAlbum_Alcarmen.AssemblyInfo.cs
+++ PhotoAlbum_Alcarmen/obj/Debug/net8.0/PhotoAlbum_Alcarmen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6e01de471461febcde50d71ff0a415476cafb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
