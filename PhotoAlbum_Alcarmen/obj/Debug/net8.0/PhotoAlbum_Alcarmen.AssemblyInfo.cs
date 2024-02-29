0569b2d33bf44b1c54cb80aed7302ef8e00ff842
Alcarmen <alcarmenbrandon@gmail.com>
2024-02-29 20:05:45 +0800

    PhotoAlbum_69
    PRogress4

--- PhotoAlbum_Alcarmen/obj/Debug/net8.0/PhotoAlbum_Alcarmen.AssemblyInfo.cs
+++ PhotoAlbum_Alcarmen/obj/Debug/net8.0/PhotoAlbum_Alcarmen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603ef7a3665ca0e22f940ea55bf1e28958784abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoAlbum_Alcarmen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
